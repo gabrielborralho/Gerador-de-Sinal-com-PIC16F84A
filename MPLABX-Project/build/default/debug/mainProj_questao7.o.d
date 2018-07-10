f478e207de4bcecea18ef94cb42e7d8a71bd3da7
Gabriel Borralho <32739349+gabrielborralho@users.noreply.github.com>
2018-07-10 01:26:16 -0300

    SG-PIC16F84A

---
+++ MPLABX-Project/build/default/debug/mainProj_questao7.o.d
@@ -0,0 +1,3 @@
+build/default/debug/mainProj_questao7.o:  \
+	C:/Users/Gabriel\ Borralho/MPLABXProjects/QUESTAO_7.X/mainProj_questao7.asm  \
+
---
+++ MPLABX-Project/build/default/production/mainProj_questao7.o.d
@@ -0,0 +1,3 @@
+build/default/production/mainProj_questao7.o:  \
+	C:/Users/Gabriel\ Borralho/MPLABXProjects/QUESTAO_7.X/mainProj_questao7.asm  \
+
